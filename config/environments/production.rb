dc8f867d719a2af639a8845513852eb5d0eff1d7
Maropost India Dev 17 <maropost@173-168-101-65.res.bhn.net>
2018-01-03 13:58:01 +0530

    heroku congig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,21 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Movie_app_#{Rails.env}"
+  config.read_encrypted_secrets = true
+   config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+      # enable_starttls_auto: true,
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      user_name: 'rajeshnuthalapati499@gmail.com',
+      password: '12851A0440'
+    }
   config.action_mailer.perform_caching = false
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -88,4 +102,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'https://raj2.herokuapp.com/'  }
 end
