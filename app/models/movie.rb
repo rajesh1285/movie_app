485a64480a22cb3acc1815aa9bec3e6f6bfd45dc
Maropost India Dev 17 <maropost@173-168-101-65.res.bhn.net>
2018-01-05 14:58:46 +0530

    changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,9 @@ class Movie < ApplicationRecord
 	has_many :views ,dependent: :destroy
     has_many :reviews ,dependent: :destroy
 	mount_uploader :image, ImageUploader
+	 
+      validates :rating, numericality: { less_than_or_equal_to: 10 }
+    validates :title,:genr,:plot,:image,:rating,:website,:year, presence: true
       
  def self.search(search)
   where("title LIKE ?", "%#{search}%") 
