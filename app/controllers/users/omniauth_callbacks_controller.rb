3288f41b45a87a267b33a2b515378ca27726c397
Maropost India Dev 17 <maropost@173-168-100-34.res.bhn.net>
2018-01-08 15:48:22 +0530

    Changes Omni Controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
- def facebook
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -12,7 +12,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-   def failure
-     redirect_to root_path
-   end
+  def failure
+    redirect_to root_path
+  end
 end
